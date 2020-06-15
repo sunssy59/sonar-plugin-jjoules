19f6006860c9c803737fce395f220eb050c3afb9
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-15 18:39:17 +0200

    for static page access

--- src/main/resources/static/jjoules_page.js
+++ src/main/resources/static/jjoules_page.js
@@ -2,6 +2,6 @@ window.registerExtension('jjoules/jjoules_page',function(options){
 	projectName = options.component.key.substring(options.component.key.indexOf(':')+1);
 
 	options.el.textContent = "";
-	Console.log("j'entre bien!!");
+	console.log("j'entre bien!!");
 
 });
