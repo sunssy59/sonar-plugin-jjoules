ef52b4de9beb28df602fde75d14846828a0c460e
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-16 01:28:37 +0200

    fix:error compile

--- src/main/resources/static/jjoules_page.js
+++ src/main/resources/static/jjoules_page.js
@@ -5,7 +5,7 @@ window.registerExtension('jjoules/jjoules_page',function(options){
 	loadAllCss();
 	loadD3JS();
 	loadAllJSScript();
-	
+
 	console.log(projectName);
 	console.log(options);
 	divToInsert = options.el;
@@ -150,6 +150,42 @@ var loadAllCss = function () {
         document.head.appendChild(link);
     });
 };
+
+/**
+ * Add the principal div to the  HTML
+ * the divForInsertingMenu
+ * the divForInsertingTest
+ * the divForChart
+ */
+var establishDesign = function(){
+    var globalDiv = document.createElement('div');
+    globalDiv.setAttribute('class', 'row');
+
+    divForInsertingMenu = document.createElement("div");
+    divForInsertingMenu.setAttribute('class', 'menu-fixed col-3 margin-left');
+
+    var rightDiv = document.createElement("div");
+    rightDiv.setAttribute('class', 'col-9');
+
+    divForInsertingTest = document.createElement("div");
+    divForInsertingTest.setAttribute('class', 'margin-top padding-left');
+
+    divForChart = document.createElement("div");
+    divForChart.setAttribute('class', 'margin-top');
+
+    rightDiv.appendChild(divForInsertingTest);
+    rightDiv.appendChild(divForChart);
+
+    var divUseless = document.createElement("div");
+    divUseless.setAttribute('class', 'col-3');
+
+    globalDiv.appendChild(divUseless);
+    globalDiv.appendChild(divForInsertingMenu);
+    globalDiv.appendChild(rightDiv);
+
+    divToInsert.appendChild(globalDiv);
+};
+
 
 // const URL_LOADED_HTML_FILE = [
 //     //"/static/jjoules/view/html/selectList.html",
