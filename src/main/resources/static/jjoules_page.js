0ab31437c0ac3ea3878ec6d31368abccbd5957d2
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-15 18:08:28 +0200

    for static page access

--- src/main/resources/static/jjoules_page.js
+++ src/main/resources/static/jjoules_page.js
@@ -1,4 +1,4 @@
-window.registerExtension('jjoules/jjoules_page',funxtion(options){
+window.registerExtension('jjoules/jjoules_page',function(options){
 	projectName = options.component.key.substring(options.component.key.indexOf(':')+1);
 
 	options.el.textContent = "";
