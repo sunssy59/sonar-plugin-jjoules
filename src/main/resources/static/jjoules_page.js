02adcb7da6c7732a9239745f4306e628779d35b0
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-16 23:43:07 +0200

    fix: modif structure

--- src/main/resources/static/jjoules_page.js
+++ src/main/resources/static/jjoules_page.js
@@ -254,17 +254,17 @@ var createGraph = function (ctx, type, allMethods,allEnergies,className) {
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                'rgba(255, 92, 132, 0.2)',
-                'rgba(54, 152, 235, 0.2)',
-                'rgba(255, 226, 86, 0.2)'
+                'rgba(25, 92, 132, 0.2)',
+                'rgba(254, 152, 235, 0.2)',
+                'rgba(225, 226, 86, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(255, 92, 132, 1)',
-                'rgba(54, 152, 235, 1)',
-                'rgba(255, 226, 86, 1)'
+                'rgba(25, 92, 132, 1)',
+                'rgba(254, 152, 235, 1)',
+                'rgba(225, 226, 86, 1)'
             ],
             borderWidth: 1,
             barPercentage: 0.2,
@@ -280,7 +280,14 @@ var createGraph = function (ctx, type, allMethods,allEnergies,className) {
                     beginAtZero: true
                 }
             }]
-        }
+        },layout: {
+            padding: {
+                left: 50,
+                right: 50,
+                top: 0,
+                bottom: 0
+            }
+
     }
 });
     //console.log(c);
