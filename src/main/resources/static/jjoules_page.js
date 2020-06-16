2c0232fbde6376a253359c5c422232becfb1d3f1
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-16 23:23:16 +0200

    fix: modif structure

--- src/main/resources/static/jjoules_page.js
+++ src/main/resources/static/jjoules_page.js
@@ -33,6 +33,15 @@ var jjoulesData = [{
 		{testName:"testOne",
 		energy:20,
 		duration: 30},
+        {testName:"testOne",
+        energy:20,
+        duration: 30},
+        {testName:"testOne",
+        energy:20,
+        duration: 30},
+        {testName:"testOne",
+        energy:20,
+        duration: 30},
 		{testName:"testTwo",
 		energy:50,
 		duration: 130},
@@ -244,12 +253,18 @@ var createGraph = function (ctx, type, allMethods,allEnergies,className) {
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 92, 132, 0.2)',
+                'rgba(54, 152, 235, 0.2)',
+                'rgba(255, 226, 86, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 92, 132, 1)',
+                'rgba(54, 152, 235, 1)',
+                'rgba(255, 226, 86, 1)'
             ],
             borderWidth: 1,
             barPercentage: 0.2,
