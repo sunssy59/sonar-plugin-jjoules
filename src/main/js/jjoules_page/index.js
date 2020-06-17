ff1acd75ae11d62f0e7bc8d8977f2351121c8028
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-17 23:00:12 +0200

    add: css

--- src/main/js/jjoules_page/index.js
+++ src/main/js/jjoules_page/index.js
@@ -8,15 +8,24 @@ window.registerExtension("jjoules/jjoules_page",options =>{
 });
 
 var data = {
-	classesNames: ["classOne","classTwo"],
+	classesNames: ["classOne","classTwo","testTree"],
 	data: [
 		[{testName:"test1One",energy:10,duration:13},
 		  {testName:"test1Two",energy:15,duration:10},
 		  {testName:"test1Tree",energy:20,duration:23}
+		  {testName:"test1Tree",energy:30,duration:23},
+		  {testName:"test1Tree",energy:20,duration:23}
 		],
 		[{testName:"test2One",energy:12,duration:23},
 		  {testName:"test2Two",energy:18,duration:8},
-		  {testName:"test2Tree",energy:30,duration:23}
+		  {testName:"test2Tree",energy:30,duration:23},
+		  {testName:"test2Tree",energy:20,duration:23}
+		],
+		[{testName:"test3One",energy:12,duration:23},
+		  {testName:"test3Two",energy:18,duration:8},
+		  {testName:"test3Tree",energy:30,duration:23},
+		  {testName:"test3Tree",energy:20,duration:23},
+		  {testName:"test33Tree",energy:20,duration:23}
 		]
 	]
 };
