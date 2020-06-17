d1791e44f1c3b4116ccb9b8148d1b67aff6d35f0
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-18 01:09:49 +0200

    try: canvas for graph using Chart.js

--- src/main/js/jjoules_page/index.js
+++ src/main/js/jjoules_page/index.js
@@ -4,7 +4,9 @@ import "../style.css";
 import AllTests from "./components/DisplayHistroyJjoules";
 
 window.registerExtension("jjoules/jjoules_page",options =>{
+	console.log(options.el);
 	return <AllTests data={data} />;
+
 });
 
 var data = {
@@ -13,20 +15,20 @@ var data = {
 		[{testName:"test1One",energy:10,duration:13},
 		  {testName:"test1Two",energy:15,duration:10},
 		  {testName:"test1Tree",energy:20,duration:23},
-		  {testName:"test1Tree",energy:30,duration:23},
-		  {testName:"test1Tree",energy:20,duration:23}
+		  {testName:"test1Four",energy:30,duration:23},
+		  {testName:"test1Five",energy:20,duration:23}
 		],
 		[{testName:"test2One",energy:12,duration:23},
 		  {testName:"test2Two",energy:18,duration:8},
-		  {testName:"test2Tree",energy:30,duration:23},
-		  {testName:"test2Tree",energy:20,duration:23}
+		  {testName:"test2Four",energy:30,duration:23},
+		  {testName:"test2Five",energy:20,duration:23}
 		],
 		[{testName:"test3One",energy:12,duration:23},
 		  {testName:"test3Two",energy:18,duration:8},
 		  {testName:"test3Tree",energy:30,duration:23},
-		  {testName:"test3Tree",energy:20,duration:23},
-		  {testName:"test33Tree",energy:20,duration:23},
-		  {testName:"test3Tree",energy:30,duration:23}
+		  {testName:"test3Four",energy:20,duration:23},
+		  {testName:"test3Five",energy:20,duration:23},
+		  {testName:"test3Six",energy:30,duration:23}
 		]
 	]
 };
