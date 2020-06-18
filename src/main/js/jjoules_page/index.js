1039c31203918753543c9de5994441069f59ee27
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-18 10:54:35 +0200

    try: insert canvas in dom

--- src/main/js/jjoules_page/index.js
+++ src/main/js/jjoules_page/index.js
@@ -46,36 +46,30 @@ var data = {
 };
 
 var createGraph = function (ctx /*,type, allMethods,allEnergies,className*/) {
-    var c = new Chart(ctx, {
+    new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["classOne","classTwo","classTree"],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: "test",
-            data: [1,3,10],
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
-                // ,
-                // 'rgba(25, 92, 132, 0.2)',
-                // 'rgba(254, 152, 235, 0.2)',
-                // 'rgba(225, 226, 86, 0.2)'
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-                // ,
-                // 'rgba(25, 92, 132, 1)',
-                // 'rgba(254, 152, 235, 1)',
-                // 'rgba(225, 226, 86, 1)'
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1,
-            barPercentage: 0.2,
-            //barThickness: 6,
-            //maxBarThickness: 8,
-            //minBarLength: 2
+            borderWidth: 1
         }]
     },
     options: {
@@ -85,13 +79,6 @@ var createGraph = function (ctx /*,type, allMethods,allEnergies,className*/) {
                     beginAtZero: true
                 }
             }]
-        },layout: {
-            padding: {
-                left: 50,
-                right: 0,
-                top: 0,
-                bottom: 0
-            }
         }
     }
 });
