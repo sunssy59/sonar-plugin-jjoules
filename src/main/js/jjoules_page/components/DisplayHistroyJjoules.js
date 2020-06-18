882731d94587fd2edaaa6c562ae420982acddc36
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-18 12:59:31 +0200

    try: bubble custom

--- src/main/js/jjoules_page/components/DisplayHistroyJjoules.js
+++ src/main/js/jjoules_page/components/DisplayHistroyJjoules.js
@@ -1,7 +1,7 @@
 import React from "react";
 // exposes React components exposed by SonarQube.
 import { DeferredSpinner } from "sonar-components";
-import { Doughnut, Bar } from 'react-chartjs-2';
+import { Doughnut, Bar,Bubble } from 'react-chartjs-2';
 
 var data = {
         labels: ["classOne","classTwo","classTree"],
@@ -50,6 +50,33 @@ var data = {
             }
         }
     }
+
+const datab = {
+  labels: ['January'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [{x:10,y:20,r:5}]
+    }
+  ]
+};
 
 function Method(props) {
 	return (
@@ -153,6 +180,7 @@ export default class AllTests extends React.Component {
 					</div>
 				)}
 				<Bar data={data} options={options}/>
+				<Bubble data={data}/>
 			</div>
 		)
 	}
