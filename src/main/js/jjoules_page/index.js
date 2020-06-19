1594a5304197d73939ccdafc1f6271667db01a71
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-19 16:20:16 +0200

    geting resurlt request /api/issues/search with sonarrequest

--- src/main/js/jjoules_page/index.js
+++ src/main/js/jjoules_page/index.js
@@ -4,13 +4,19 @@ import "../style.css";
 import AllTests from "./components/DisplayHistroyJjoules";
 
 window.registerExtension("jjoules/jjoules_page",options =>{
-	console.log(options);
+	window.SonarRequest.getJSON('/api/issues/search', {
+        resolved: false,
+        componentKeys: options.component.key
+    }).then(function (arg) {
+    	console.log(arg);
+    });
+	//console.log(options);
 	return <AllTests data={transformData(data1)} />;
 
 });
 
 var data = {
-	classesNames: ["classOne","classTwo","classTree"],
+	classesNames: ["ClassOne","ClassTwo","ClassTree"],
 	data: [
 		[{testName:"test1One",energy:10,duration:13},
 		  {testName:"test1Two",energy:15,duration:10},
@@ -41,7 +47,7 @@ function transformData(data){
 }
 var data1 = [
 	{
-	className: "classOne",
+	className: "ClassOne",
 	methods: [{testName:"test1OneTest",energy:10,duration:13},
 		  {testName:"test1Two",energy:15,duration:10},
 		  {testName:"test1Tree",energy:20,duration:23},
@@ -51,7 +57,7 @@ var data1 = [
 
 	},
 	{
-	className: "classTwo",
+	className: "ClassTwo",
 	methods: [{testName:"test2One",energy:12,duration:23},
 		  {testName:"test2Two",energy:18,duration:8},
 		  {testName:"test2Four",energy:30,duration:23},
@@ -59,7 +65,7 @@ var data1 = [
 		]
 	},
 	{
-	className: "classTree",
+	className: "ClassTree",
 	methods: [{testName:"test3One",energy:12,duration:23},
 		  {testName:"test3Two",energy:18,duration:8},
 		  {testName:"test3Tree",energy:30,duration:23},
