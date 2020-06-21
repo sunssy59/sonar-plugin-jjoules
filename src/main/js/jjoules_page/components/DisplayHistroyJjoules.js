72a9c91aca02c81816fd07748a6d80635e22d251
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-21 19:56:04 +0200

    test:Rating class

--- src/main/js/jjoules_page/components/DisplayHistroyJjoules.js
+++ src/main/js/jjoules_page/components/DisplayHistroyJjoules.js
@@ -2,13 +2,17 @@ import React from "react";
 // exposes React components exposed by SonarQube.
 import { DeferredSpinner } from "sonar-components";
 import { Bar } from 'react-chartjs-2';
+import { Rating } from "../../components/Rating";
 import { randomColor, createDataGraph, options } from "../utils/utils";
 
 
 function Method(props) {
 	return (
 		<div className="result list-inline-item">
-			<h6 className="method overview-panel-title" >Test: {props.testName}</h6>
+			<h6 className="method overview-panel-title" >
+				Test: {props.testName}
+				<Rating rating=1.0 />
+			</h6>
 			<p>Energy: {props.energy} </p>
 			<p>Duration: {props.duration} </p>
 		</div>
