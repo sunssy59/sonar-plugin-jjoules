6dbc992b143b8a1b17b97b1da51be52aa31a63b3
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-26 16:17:53 +0200

    add: graph icone

--- src/main/js/jjoules_page/components/DisplayHistroyJjoules.js
+++ src/main/js/jjoules_page/components/DisplayHistroyJjoules.js
@@ -26,6 +26,19 @@ function Method(props) {
 
 	);
 }
+
+function GraphSVG(props){
+	return (
+		<svg class="svg-icon svg-box-magnifier" 
+			viewBox="0 0 16 16" 
+			height="16" 
+			width="16">
+			<path 
+				d="M17.431,2.156h-3.715c-0.228,0-0.413,0.186-0.413,0.413v6.973h-2.89V6.687c0-0.229-0.186-0.413-0.413-0.413H6.285c-0.228,0-0.413,0.184-0.413,0.413v6.388H2.569c-0.227,0-0.413,0.187-0.413,0.413v3.942c0,0.228,0.186,0.413,0.413,0.413h14.862c0.228,0,0.413-0.186,0.413-0.413V2.569C17.844,2.342,17.658,2.156,17.431,2.156 M5.872,17.019h-2.89v-3.117h2.89V17.019zM9.587,17.019h-2.89V7.1h2.89V17.019z M13.303,17.019h-2.89v-6.651h2.89V17.019z M17.019,17.019h-2.891V2.982h2.891V17.019z">
+			</path>
+		</svg>
+	);
+}
 /**
  * Create HTML representation of class test
  * @return HTML code of one test class containt there method representation
@@ -59,12 +72,7 @@ React.Component{
 						onMouseEnter={this.changePathFillToWhite}
 						onMouseLeave={this.changePathFillToBlue}
 						>  
-						<svg class="svg-icon svg-box-magnifier" 
-							viewBox="0 0 16 16" 
-							height="16" 
-							width="16">
-							<path d="M17.431,2.156h-3.715c-0.228,0-0.413,0.186-0.413,0.413v6.973h-2.89V6.687c0-0.229-0.186-0.413-0.413-0.413H6.285c-0.228,0-0.413,0.184-0.413,0.413v6.388H2.569c-0.227,0-0.413,0.187-0.413,0.413v3.942c0,0.228,0.186,0.413,0.413,0.413h14.862c0.228,0,0.413-0.186,0.413-0.413V2.569C17.844,2.342,17.658,2.156,17.431,2.156 M5.872,17.019h-2.89v-3.117h2.89V17.019zM9.587,17.019h-2.89V7.1h2.89V17.019z M13.303,17.019h-2.89v-6.651h2.89V17.019z M17.019,17.019h-2.891V2.982h2.891V17.019z"></path>
-						</svg>
+						<GraphSVG />
 
 						<strong>&darr;</strong>
 					</button>
@@ -105,7 +113,7 @@ React.Component {
 		for(let el of document.getElementsByClassName("canvas"))
 			el.hidden = true;
 		for(let el of document.querySelectorAll("button[id^='but-']"))
-			el.innerHTML = "graph <strong>&darr;</strong>";
+			el.lastChild.innerHTML = "<strong>&darr;</strong>";
 	}
 	render() {
 		return (
@@ -165,16 +173,16 @@ export default class AllTests extends React.Component {
 		}
 		var but = document.getElementById("but-"+className);
 		for(let el of document.querySelectorAll("button[id^='but-']"))
-			el.innerHTML = "graph <strong>&darr;</strong>";
+			el.lastChild.innerHTML = "<strong>&darr;</strong>";
 
 		if (statBefore) {
 			but.removeAttribute("class");
 			but.setAttribute("class","button button-active");
-			but.innerHTML = "graph <strong>&uarr;</strong>";
+			but.lastChild.innerHTML = "<strong>&uarr;</strong>";
 		}else{
 			but.removeAttribute("class");
 			but.setAttribute("class","button");
-			but.innerHTML = "graph <strong>&darr;</strong>";
+			but.lastChild.innerHTML = "<strong>&darr;</strong>";
 		}
 		
 	}
